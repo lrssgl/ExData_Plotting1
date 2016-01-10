caac0da93b81a0a0a22cc59643ce9baca51855ee
Larisa Segal <larisasegal@yahoo.com>
2016-01-10 17:14:40 -0500

    plot 4 source and png files

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", temp)
-data <- read.table(unz(temp, "household_power_consumption.txt"), sep=";", header=TRUE, colClasses = c("factor","factor","numeric","numeric","numeric","numeric","numeric","numeric","factor"), na.strings=c("?"))
+data <- read.table(unz(temp, "household_power_consumption.txt"), sep=";", header=TRUE, colClasses = c("factor","factor","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), na.strings=c("?"))
 unlink(temp)
 
 mydata<-subset(data,(as.Date(Date, format="%d/%m/%Y")==as.Date("2007-02-01")| as.Date(Date, format="%d/%m/%Y")==as.Date("2007-02-02")) )
